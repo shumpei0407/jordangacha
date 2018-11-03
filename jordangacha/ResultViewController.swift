ab0be14c2ae442937f982f722b484b4d940b03ee
田口峻平 <taguchi@lifeistech.co.jp>
2018-11-03 23:51:46 +0900

    未完成ver

---
+++ jordangacha/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  jordangacha
+//
+//  Created by 田口峻平 on 2018/11/03.
+//  Copyright © 2018年 田口峻平. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var number: Int!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var jordanImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
