99afcbe2f75027cf310d642e174a168a74a3e381
田口峻平 <taguchi@lifeistech.co.jp>
2018-11-03 19:32:38 +0900

    Initial Commit

---
+++ jordangacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jordangacha
+//
+//  Created by 田口峻平 on 2018/11/03.
+//  Copyright © 2018年 田口峻平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
